d74fd4af1feda8ae31fffb46b38d7951fad11b30
Danang Aji Bimantoro <adjie6661@gmail.com>
2018-02-26 10:12:03 +0700

    share it

---
+++ share.php
@@ -0,0 +1,17 @@
+<?php
+
+ $s_penelitian = strtotime('2018-01-17 00:00:00');
+    $e_penelitian = strtotime('2019-01-16 00:00:00');
+
+    $s_semester   = strtotime('2018-09-01 00:00:00');
+    $e_semester   = strtotime('2019-01-31 00:00:00');
+
+    if(($s_penelitian <= $s_semester || $s_penelitian >= $s_semester) && ($e_penelitian <= $e_semester || $e_penelitian >= $e_semester) && ($e_penelitian > $s_semester)){
+    	$masuk = 'masuk';
+    }else{
+    	$masuk = 'tidak masuk';
+    }
+
+    echo $masuk;
+
+?>
